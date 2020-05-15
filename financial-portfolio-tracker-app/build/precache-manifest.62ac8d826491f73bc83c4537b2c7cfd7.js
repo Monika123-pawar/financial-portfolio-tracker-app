7546ae40f36198b62a3a203c757c728524c3777f
Monika Pawar <58398532+Monika123-pawar@users.noreply.github.com>
2020-05-15 20:14:16 +0530

    Add files via upload

---
+++ financial-portfolio-tracker-app/build/precache-manifest.62ac8d826491f73bc83c4537b2c7cfd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b8301a26c535d4afba3258aef8fe62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd39701b7531fb2e71e",
+    "url": "/static/css/main.994927ba.chunk.css"
+  },
+  {
+    "revision": "b3fd4a2435359d160a82",
+    "url": "/static/js/2.0fe5c3d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fe5c3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd39701b7531fb2e71e",
+    "url": "/static/js/main.6ab30a8f.chunk.js"
+  },
+  {
+    "revision": "67bfe6e155e14285a39b",
+    "url": "/static/js/runtime-main.89dd57ce.js"
+  }
+]);
